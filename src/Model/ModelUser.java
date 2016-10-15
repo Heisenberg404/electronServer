7edefd844a1703f7679daaa54978e79489b66272
davidrios9 <davidrs1277@gmail.com>
2016-10-15 14:46:59 -0500

    Login,logout,and check sessions with security

--- src/Model/ModelUser.java
+++ src/Model/ModelUser.java
@@ -1,10 +1,14 @@
 package Model;
 
+import java.io.IOException;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 
 import Beans.Products;
 import Beans.Users;
@@ -102,4 +106,48 @@ public class ModelUser {
         String back = cs.getString(2);
         System.out.println(back);
 	}
+	
+    public void iniciarSesion(Users user) throws Exception {
+        Connection con = conn.getConnection();
+        CallableStatement cs = null;
+        cs = con.prepareCall("{call PKG_Users.QUERYUSER(?,?,?)}");
+        // Parametros del procedimiento almacenado
+        cs.setString(1, user.getUsername());
+        cs.setString(2, user.getPass());
+        // Definimos los tipos de los parametros de salida del procedimiento
+        // almacenado
+        cs.registerOutParameter(3, java.sql.Types.VARCHAR);
+        cs.execute();
+        // Se obtienen la salida del procedimineto almacenado     
+        String back = cs.getString(3);
+        System.out.println(back);       
+        System.out.println(back);    
+        
+        if (back.equals("YES")) {
+
+            FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("usuario", user);
+            FacesContext.getCurrentInstance().getExternalContext().redirect("PrincipalMenu.xhtml");
+        } else {
+        	
+            FacesContext.getCurrentInstance().addMessage(null , new FacesMessage(FacesMessage.SEVERITY_WARN,"Error","User no existe")); 
+        }
+
+    }
+    
+    public void checkSession() {
+        try {
+            Users us = (Users) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("usuario");
+            if (us == null) {
+                FacesContext.getCurrentInstance().getExternalContext().redirect("LoginUser.xhtml");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    
+    public void closeSession() throws IOException{        
+//        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().clear();
+        FacesContext.getCurrentInstance().getExternalContext().invalidateSession();    
+    }
+
 }
